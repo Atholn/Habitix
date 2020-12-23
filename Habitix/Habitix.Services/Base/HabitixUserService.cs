547a1233215ca62b58c54b9504a333b577515117
m4t3n5z <mz3pr0gr4mmm3r@gmail.com>
2020-12-23 21:26:31 +0100

    Add Base Auth base 64

---
+++ Habitix/Habitix.Services/Base/HabitixUserService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Habitix.Core.Models;
+using Habitix.Data.Repositories.Interfaces;
+using Habitix.Services.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Habitix.Services.Base
+{
+    public class HabitixUserService : IHabitixUserService
+    {
+        private readonly IHabitixUserRepository _habitixUserRepository;
+        private readonly IMapper _mapper;
+
+        public HabitixUserService(IHabitixUserRepository habitixUserRepository, IMapper mapper)
+        {
+            _habitixUserRepository = habitixUserRepository;
+            _mapper = mapper;
+        }
+        public long Create(HabitixUserRepresentation habitixUserRepresentation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HabitixUserRepresentation> Get(long id)
+        {
+            return await Task.Run(() =>
+           {
+               return 
+           });
+        }
+    }
+}
---
+++ Habitix/Habitix.Services/Base/Interfaces/IHabitixUserService.cs
@@ -0,0 +1,18 @@
+﻿using Habitix.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Habitix.Services.Base.Interfaces
+{
+    interface IHabitixUserService
+    {
+
+        public long Create(HabitixUserRepresentation habitixUserRepresentation);
+        public HabitDateRepresentation Get(long id);
+      //  Task<IEnumerable<ProductDTO>> GetList(long companyId);
+      //  Task<ProductDeleteResponse> Delete(long id, long companyId);
+     //   Task<ProductUpdateResponse> Update(ProductCreateRequest request, long productId, long companyId);
+    }
+}
