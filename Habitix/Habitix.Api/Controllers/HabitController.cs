5cb9f05a0acd8adaa2af51260ca240844ebf64ae
m4t3n5z <mz3pr0gr4mmm3r@gmail.com>
2021-01-02 16:49:08 +0100

    Add HabitController

---
+++ Habitix/Habitix.Api/Controllers/HabitController.cs
@@ -0,0 +1,23 @@
+﻿using Habitix.Services.Base.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Habitix.Api.Controllers
+{
+    [Route("api/habitixUser/{habitixUserId}/habit")]
+    [ApiController]
+    public class HabitController : ControllerBase
+    {
+        private readonly IHabitixUserService _habitixUserService;
+
+        public HabitController(IHabitixUserService habitixUserService)
+        {
+            _habitixUserService = habitixUserService;
+        }
+
+    }
+}
