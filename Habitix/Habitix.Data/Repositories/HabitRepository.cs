d96d1992bd45ae7c79792f1be5a843aac4832161
m4t3n5z <mz3pr0gr4mmm3r@gmail.com>
2021-01-08 08:22:51 +0100

    Add Habit repo and interface

---
+++ Habitix/Habitix.Data/Repositories/HabitRepository.cs
@@ -0,0 +1,26 @@
+﻿using Habitix.Data.Models;
+using Habitix.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Habitix.Data.Repositories
+{
+    public class HabitRepository : BaseRepository<Habit>, IHabitRepository
+    {
+        public HabitRepository(BaseContext context) : base(context)
+        {
+
+        }
+
+        public IEnumerable<Habit> GetListByUserId(long id)
+        {
+            return  context.Habits
+                    .Include(h => h.HabitixUserId == id)
+                    .AsEnumerable();
+             
+        }
+    }
+}
---
+++ Habitix/Habitix.Data/Repositories/Interfaces/IHabitRepository.cs
@@ -0,0 +1,12 @@
+﻿using Habitix.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habitix.Data.Repositories.Interfaces
+{
+    public interface IHabitRepository : IBaseRepository<Habit>
+    {
+        IEnumerable<Habit> GetListByUserId(long id);
+    }
+}
