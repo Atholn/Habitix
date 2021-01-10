9b1f6c439412d2ba263debd722e8b84eeb28d8d2
m4t3n5z <mz3pr0gr4mmm3r@gmail.com>
2021-01-10 12:13:40 +0100

    Add Helper claims principal Extensions

---
+++ Habitix/Habitix.Api/Helpers/ExtensionsMethods/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Habitix.Api.Helpers.ExtensionsMethods
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal principal)
+        {
+            return principal.Claims.FirstOrDefault(c => c.Type == "id").Value;
+        }
+    }
+}
