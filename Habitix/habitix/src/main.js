c71925deec6895747dd0a2888e4756817fe2aa65
m4t3n5z <mz3pr0gr4mmm3r@gmail.com>
2021-02-10 11:14:17 +0100

    Add vuetify.js to project

--- Habitix/habitix/src/main.js
+++ Habitix/habitix/src/main.js
@@ -1,16 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from "./router/index.js";
+
 // import store from "./store";
 //import vuetify from "./plugins/vuetify";
 import axios from "./plugins/axios"
+
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+
   // store,
   // vuetify,
   axios,
-  render: h => h(App),
+
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
