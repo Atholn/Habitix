5af7e8d00f68c2c30266821b060a744b0b7e67d9
m4t3n5z <mz3pr0gr4mmm3r@gmail.com>
2021-04-14 23:43:17 +0200

    Add test animation

--- Habitix/habitix/src/views/Home.vue
+++ Habitix/habitix/src/views/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="home">
     <v-container fluid fill-height>
-     Hello in Habitix!
+     <div style="width: 100%; text-align: center;"> Hello in Habitix!</div>
+     <div>
+       <example/>
+     </div>
+     
     </v-container>
   </div>
 </template>
 
 <script>
+import example from "../Animations/example"
+
 export default {
   name: "Home",
   components: {
+    example,
   },
   data() {
     return {
